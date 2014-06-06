d6d759106f638f8b109ec2a0383f020ef9d3902a
Javier Aguilar <mjolnic@gmail.com>
2014-06-06 19:40:24 +0200

    combine core providers, fix namespace path

--- src/Thor/ThorServiceProvider.php
+++ src/Thor/ThorServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace Thor;
 
+use Illuminate\Support\Facades;
 use Illuminate\Support\ServiceProvider,
-    View, Doc;
+    View,
+    Doc;
 
 class ThorServiceProvider extends ServiceProvider
 {
@@ -12,7 +14,7 @@ class ThorServiceProvider extends ServiceProvider
      *
      * @var bool
      */
-    protected $defer = false;
+    //protected $defer = false;
 
     /**
      * Bootstrap the application events.
@@ -21,8 +23,12 @@ class ThorServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->package('thor/framework', 'thor');
+        $this->package('thor/framework', 'thor', realpath(__DIR__.'/../'));
         
+        Facades\Lang::swap($this->app['thor.translator']);
+        Facades\Route::swap($this->app['thor.router']);
+        Facades\URL::swap($this->app['thor.url']);
+
         // Always expose the current view name and all the Document vars
         View::composer('*', function($view) {
             Doc::view($view->getName());
@@ -38,6 +44,10 @@ class ThorServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->registerTranslator();
+        $this->registerRouter();
+        $this->registerUrlGenerator();
+        $this->registerPublishCommand();
         $this->registerBag();
         $this->registerDocument();
     }
@@ -49,7 +59,64 @@ class ThorServiceProvider extends ServiceProvider
      */
     public function provides()
     {
-        return array('thor.bag', 'thor.document');
+        return array(
+            'thor.translator',
+            'thor.router',
+            'thor.url',
+            'thor.lang_publisher',
+            'thor.bag',
+            'thor.document'
+        );
+    }
+
+    /**
+     *
+     * @return void
+     */
+    protected function registerTranslator()
+    {
+        $this->app->bindShared('thor.translator', function($app) {
+            return new I18n\Translator($app);
+        });
+    }
+
+    /**
+     *
+     * @return void
+     */
+    protected function registerRouter()
+    {
+        $this->app->bindShared('thor.router', function($app) {
+            return new I18n\Router($app['events'], $app);
+        });
+    }
+
+    /**
+     *
+     * @return void
+     */
+    protected function registerUrlGenerator()
+    {
+        $this->app->bindShared('thor.url', function($app) {
+            return new I18n\UrlGenerator($app['thor.router']->getRoutes(), $app->rebinding('request', function ($app, $request) {
+                        $app['thor.url']->setRequest($request);
+                    }));
+        });
+    }
+
+    protected function registerPublishCommand()
+    {
+        $this->app->bindShared('thor.lang_publisher', function($app) {
+            return new I18n\LangPublisher($app['files'], $app['path'] . '/lang');
+        });
+
+        $this->app->bindShared('command.lang.publish', function($app) {
+            return new I18n\LangPublishCommand($app['thor.lang_publisher']);
+        });
+
+        $this->commands(array(
+            'command.lang.publish'
+        ));
     }
 
     /**
